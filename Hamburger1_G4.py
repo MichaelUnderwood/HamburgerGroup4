1da81d27a101c4dc6e5eebc0a3d6d3a037cff0dc
Michael Underwood <underwoodmichael6@gmail.com>
2023-02-28 14:13:41 -0700

    main.commit.1.3

--- Hamburger1_G4.py
+++ Hamburger1_G4.py
@@ -2,10 +2,17 @@
 # Lily Pettit, Elliot Pi, Noelle Burton, Preston Fitzgerald
 
 # THIS IS THE MASTER - DO NOT DELETE WORK
+# WORK WITHIN YOUR OWN BRANCH - WILL WILL MERGE CAHNGES WHEN WE EACH HAVE OUR PORTIONS DONE 
+# ESSENTIALLY WE WILL BE COPY AND PASTING EACHOTHERS WORK FROM THE INDIVIDUAL BRANCHES AND CHECKING TO SEE IF IT WORKS. 
 
-# test for committing on GitHub
+# LILLY 
 
+# NOELLE
 
+# MICHAEL U
 
+#  This is where Preston will start working on a dictionary. Here's hoping nothing will break
 
-# This is where Preston will start working on a dictionary. Here's hoping nothing will break
+# ELLIOT
+
+# MICHAEL H
